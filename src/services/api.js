681aa6f2efb055c8872caf404a26b43c8e9a805b
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2020-08-22 23:49:09 -0300

    challenge completed

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  // Standard address to work with Android Studio Emulator
+  baseURL: 'http://10.0.2.2:3333',
 });
 
 export default api;
